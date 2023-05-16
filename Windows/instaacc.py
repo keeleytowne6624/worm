7a5608cf0dcf6da04491c152f8a8adea3b7e0788
keeleytowne6624 <133508440+keeleytowne6624@users.noreply.github.com>
2023-05-16 14:49:00 +1000

    s

--- Windows/instaacc.py
+++ Windows/instaacc.py
@@ -7,9 +7,16 @@ def generate_username():
 def generate_password():
     return ''.join(random.choices(string.ascii_letters + string.digits, k=10))
 
+def generate_email():
+    username = generate_username()
+    domain = ''.join(random.choices(string.ascii_lowercase, k=5))
+    extension = random.choice(['com', 'net', 'org'])
+    return f"{username}@{domain}.{extension}"
+
 def create_instagram_account():
     username = generate_username()
     password = generate_password()
+    email = generate_email()
 
     # Code here to automate the account creation process
     # Bypassing security measures and terms of service
@@ -17,5 +24,6 @@ def create_instagram_account():
     print("New Instagram Account Created:")
     print("Username:", username)
     print("Password:", password)
+    print("Email:", email)
 
 create_instagram_account()
